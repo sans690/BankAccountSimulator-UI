f843503f760e587434e92887edcc1edd81828422
sans690 <scsmith7255@gmail.com>
2026-03-31 15:10:32 -0400

    first commit

---
+++ BankAccountSimulator/Users.cs
@@ -0,0 +1,8 @@
+class Users
+{
+    string? firstName { get; set; }
+    string? lastName { get; set; }
+    int cardNumber { get; set; }
+    int cardPin { get; set; }
+    int balance { get; set; }
+}
