b38aedc66caeacbf3452bb73a085d62edb4c0641
sans690 <scsmith7255@gmail.com>
2026-04-01 15:19:55 -0400

    Setup of sign in, menu layout there, needs functionality

--- BankAccountSimulator/Menu.cs
+++ BankAccountSimulator/Menu.cs
@@ -6,18 +6,9 @@ using BankAccountSimulator;
 
 class Menu
 {
-
-    public static List<List<int>> userList = [[1, 111, 100], [2, 222, 50], [3, 333, 0]];
-
     public static void Run(string[] args)
     {
-        SignIn();
         SelectionOptions();
-    }
-
-    public static void SignIn()
-    {
-        
     }
 
     public static void SelectionOptions()
